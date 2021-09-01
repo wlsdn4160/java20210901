d110477cb81b5d71038632c9a757cec3944ed9cb
jimmy <wlsdn4160@gmail.com>
2021-09-01 10:37:27 +0900

    data type

---
+++ src/p01review/C01Type.java
@@ -0,0 +1,43 @@
+package p01review;
+
+public class C01Type {
+
+	public static void main(String[] args) {
+		//데이터타입
+		//1. 기본타입(primitive type)
+		//- 8개
+		//byte(1), short(2), int(3), long(8)
+		//float(4),double(8)
+		//char(2)
+		//boolean(1)
+		
+		int i = 4;
+		int arr[] = {3,4};
+		int j = i;
+		int brr[] = arr;
+		
+		brr[0] = 33;
+		System.out.println(arr[0]);
+		
+		
+		
+		//2. 참조타입(reference type)
+		// 나머지 모두
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
